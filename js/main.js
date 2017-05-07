2c36106dcf2b600f64d6556337568091a78bf1df
jennypenfield <jipenfield11@gmail.com>
2017-05-07 16:24:33 -0500

    added error checks to print error message if invalid math operations

--- js/main.js
+++ js/main.js
@@ -1,29 +1,54 @@
 /* global $ */
 
+let inputCharArray = ['']
+let totalString = ''
+let operators = ['+', '-', '*', '/']
+
 $('.btn').click(function () {
   if (this.id === 'C') {
-    input = ['']
+    inputCharArray = ['']
     updateTextField()
   } else if (this.id === 'CE') {
-    input.pop()
+    inputCharArray.pop()
     updateTextField()
   } else if (this.id === '=') {
     getTotal()
   } else {
-    input.push(this.id)
+    inputCharArray.push(this.id)
     updateTextField()
   }
 })
 
-let input = ['']
-let totalString = ''
-
 function updateTextField () {
-  totalString = input.join('')
+  totalString = inputCharArray.join('')
   $('#display').html(totalString)
 }
 
 function getTotal () {
-  totalString = input.join('')
-  $('#display').html(eval(totalString))
+  if (hasError(inputCharArray)) {
+    $('#display').html('ERROR')
+  } else {
+    totalString = inputCharArray.join('')
+    $('#display').html(eval(totalString))
+  }
+}
+
+function hasError (inputCharArray) {
+  // check for two operators in a row
+  for (let i = 1; i < inputCharArray.length - 1; i++) {
+    if (operators.indexOf(inputCharArray[i]) !== -1 && operators.indexOf(inputCharArray[i - 1]) !== -1) {
+      return true
+    }
+  }
+  // check for double decimal points
+  for (let i = 1; i < inputCharArray.length - 1; i++) {
+    if (inputCharArray[i] === '.' && inputCharArray[i - 1] === '.') {
+      return true
+    }
+  }
+  // check for an operator at the end of the equation
+  if (operators.indexOf(inputCharArray[inputCharArray.length - 1]) !== -1) {
+    return true
+  }
+  return false
 }

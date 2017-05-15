63a721a7aafd6b90e54cc5a61669155e41937c1b
jennypenfield <jipenfield11@gmail.com>
2017-05-14 21:43:53 -0500

    removed eval and replaced with reduce fn

--- js/main.js
+++ js/main.js
@@ -1,82 +1,55 @@
 /* global $ */
 
-let inputCharArray = []
-let totalString = ''
-let operators = ['+', '-', '*', '/']
+let numOperArray = []
+let inputNum = ''
+const OPERATORS = ['+', '-', '*', '/']
 
-$('.btn').click(function buttonInputToArray () {
+$('.btn').click(function buttonInputCalculator () {
   if (this.id === 'C') {
-    inputCharArray = []
-    updateTextField()
+    numOperArray = []
+    displayTotal()
   } else if (this.id === 'CE') {
-    inputCharArray.pop()
-    updateTextField()
+    backspaceOneField()
   } else if (this.id === '=') {
-    getTotal()
+    numOperArray.push(inputNum)
+    displayTotal()
+    inputNum = ''
+  } else if (OPERATORS.includes(this.id)) {
+    numOperArray.push(inputNum)
+    numOperArray.push(this.id)
+    inputNum = ''
   } else {
-    inputCharArray.push(this.id)
-    updateTextField()
+    inputNum += this.id
+    updateTextField(inputNum)
   }
 })
 
-function updateTextField () {
-  totalString = inputCharArray.join('')
-  $('#display').html(totalString)
+function updateTextField (inputNum) {
+  $('#display').html(inputNum)
+}
+
+function backspaceOneField () {
+  numOperArray.pop()
+  $('#display').html(numOperArray)
+}
+
+function displayTotal () {
+  $('#display').html(getTotal())
 }
 
 function getTotal () {
-  if (hasError(inputCharArray)) {
-    inputCharArray = []
-    $('#display').html('ERROR')
-  } else {
-    totalString = inputCharArray.join('')
-    $('#display').html(eval(totalString).toFixed(10))
-  }
-}
-
-function hasError (inputCharArray) {
-  // check error conditions
-  if (hasTwoOperatorsInARow(inputCharArray)) {
-    return true
-  }
-  if (hasDoubleDecimalPoints(inputCharArray)) {
-    return true
-  }
-  if (hasOperatorBegOrEnd(inputCharArray)) {
-    return true
-  }
-  if (hasRogueDecimalPoints(inputCharArray)) {
-    return true
-  }
-  return false
-}
-
-function hasTwoOperatorsInARow (inputCharArray) {
-  for (let i = 1; i < inputCharArray.length; i++) {
-    if (operators.indexOf(inputCharArray[i]) !== -1 && operators.indexOf(inputCharArray[i - 1]) !== -1) {
-      return true
+  let total = numOperArray.reduce(function (accum, currentVal, index) {
+    let nextNum = parseFloat(numOperArray[index + 1])
+    if (currentVal === '+') {
+      accum += nextNum
+    } else if (currentVal === '-') {
+      accum -= nextNum
+    } else if (currentVal === '*') {
+      accum *= nextNum
+    } else if (currentVal === '/') {
+      accum /= nextNum
     }
-  }
-}
-
-function hasDoubleDecimalPoints (inputCharArray) {
-  for (let i = 1; i < inputCharArray.length; i++) {
-    if (inputCharArray[i] === '.' && inputCharArray[i - 1] === '.') {
-      return true
-    }
-  }
-}
-
-function hasOperatorBegOrEnd (inputCharArray) {
-  if (operators.indexOf(inputCharArray[0]) !== -1 ||
-    operators.indexOf(inputCharArray[inputCharArray.length - 1]) !== -1) {
-    return true
-  }
-}
-
-function hasRogueDecimalPoints (inputCharArray) {
-  if (inputCharArray[inputCharArray.length - 1] === '.' && operators.indexOf(inputCharArray[inputCharArray.length - 1]) !== -1 ||
-    inputCharArray[0] === '.' && operators.indexOf(inputCharArray[1]) !== -1) {
-    return true
-  }
+    return accum
+  }, parseFloat(numOperArray[0]))
+  return total
 }
